337bb91b21dc800a5fef3de014d76297154904bb
Martynas Janonis <32335421+mjanonis@users.noreply.github.com>
2020-12-01 19:26:38 +0000

    Day 1 completed

---
+++ day1-2.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <unordered_map>
+
+int main()
+{
+    std::ifstream input{"day1.in"};
+    std::ofstream output{"day1-2.out"};
+
+    std::vector<int> numbers;
+    int tmp;
+
+    while(input >> tmp){
+        numbers.push_back(tmp);
+    }
+
+    for(size_t i = 0; i < numbers.size() - 2; ++i){
+        std::unordered_map<int, int> hmap;
+        int sum = 2020 - numbers[i];
+        for(size_t j = i + 1; j < numbers.size(); ++j){
+            if(hmap.find(sum - numbers[j]) != hmap.end()){
+                output << numbers[i] * numbers[j] * (sum-numbers[j]) << '\n';
+            }
+            hmap[numbers[j]] = j;
+        }
+    }
+
+}
