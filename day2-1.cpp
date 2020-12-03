fe78da434fbfdb217af28d5a890cac07d37c7684
Martynas Janonis <martynas.janonis.17@ucl.ac.uk>
2020-12-03 19:55:37 +0000

    Day 2 part 1 completed

---
+++ day2-1.cpp
@@ -0,0 +1,25 @@
+#include <fstream>
+#include <vector>
+#include <algorithm>
+
+int main(){
+    std::ifstream input{"day2.in"};
+    std::ofstream output{"day2-1.out"};
+
+    std::string tmp;
+    unsigned int valid_pwords = 0;
+
+    while(std::getline(input, tmp)){
+
+        size_t min = std::stoi(tmp.substr(0, tmp.find('-')));
+        size_t max = std::stoi(tmp.substr(tmp.find('-')+1));
+        char ch = tmp.at(tmp.find(' ')+1);
+
+        size_t occur = std::count(tmp.begin()+tmp.find(':')+2, tmp.end(), ch);
+
+        if(occur>=min && occur<=max){++valid_pwords;}
+    }
+
+    output << valid_pwords << std::endl;
+
+}
