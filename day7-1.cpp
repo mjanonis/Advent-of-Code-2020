7e9712c0fdce54fe91059bbf3bcd3387669a0e60
Martynas Janonis <martynas.janonis.17@ucl.ac.uk>
2020-12-07 22:00:51 +0000

    Day 7 completed

---
+++ day7-1.cpp
@@ -0,0 +1,52 @@
+#include <fstream>
+#include <set>
+#include <sstream>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+void get_containing_bags(
+    const std::string& color, std::set<std::string>& contains_sgold_bag,
+    const std::unordered_map<std::string, std::set<std::string>>& contained_in)
+{
+  // Check if key exists before trying to access
+  if (contained_in.find(color) == contained_in.end()) {
+    return;
+  }
+  for (auto c : contained_in.at(color)) {
+    contains_sgold_bag.insert(c);
+    get_containing_bags(c, contains_sgold_bag, contained_in);
+  }
+}
+
+int main()
+{
+  std::ifstream input{"day7.in"};
+  std::ofstream output{"day7-1.out"};
+
+  std::string tmp;
+
+  // Maps from a bag color to where it is contained
+  std::unordered_map<std::string, std::set<std::string>> contained_in;
+  while (getline(input, tmp)) {
+    std::string color = tmp.substr(0, tmp.find("bags") - 1);
+
+    // The rest of the input (following the word contain)
+    std::string rest = tmp.substr(tmp.find("contain") + 7);
+    if (rest == " no other bags.") {
+      continue;
+    }
+    std::istringstream ss(rest);
+    std::string count_color;
+    // The strings from the stringstream will have a leading space
+    while (getline(ss, count_color, ',')) {
+      // Remove the leading space and leave just the count and the color
+      count_color = count_color.substr(1, count_color.find(" bag") - 1);
+      std::string inner_bag = count_color.substr(count_color.find(' ') + 1);
+      contained_in[inner_bag].insert(color);
+    }
+  }
+  std::set<std::string> contains_sgold_bag;
+  get_containing_bags("shiny gold", contains_sgold_bag, contained_in);
+  output << contains_sgold_bag.size() << std::endl;
+}
---
+++ day7-2.cpp
@@ -0,0 +1,55 @@
+#include <fstream>
+#include <set>
+#include <sstream>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+size_t total_bag_contents(
+    const std::string& color,
+    const std::unordered_map<
+        std::string, std::vector<std::pair<size_t, std::string>>>& contains)
+{
+  // Check if key exists before trying to access
+  if (contains.find(color) == contains.end()) {
+    return 0;
+  }
+  size_t sum = 0;
+  for (auto cc : contains.at(color)) {
+    sum += cc.first;
+    sum += cc.first * total_bag_contents(cc.second, contains);
+  }
+  return sum;
+}
+
+int main()
+{
+  std::ifstream input{"day7.in"};
+  std::ofstream output{"day7-2.out"};
+
+  std::string tmp;
+
+  // Maps from a bag color to its contents
+  std::unordered_map<std::string, std::vector<std::pair<size_t, std::string>>>
+      contains;
+  while (getline(input, tmp)) {
+    std::string color = tmp.substr(0, tmp.find("bags") - 1);
+
+    // The rest of the input (following the word contain)
+    std::string rest = tmp.substr(tmp.find("contain") + 7);
+    if (rest == " no other bags.") {
+      continue;
+    }
+    std::istringstream ss(rest);
+    std::string count_color;
+    // The strings from the stringstream will have a leading space
+    while (getline(ss, count_color, ',')) {
+      // Remove the leading space and leave just the count and the color
+      count_color = count_color.substr(1, count_color.find(" bag") - 1);
+      size_t count = std::stoi(count_color.substr(0, count_color.find(' ')));
+      std::string inner_bag = count_color.substr(count_color.find(' ') + 1);
+      contains[color].push_back(std::make_pair(count, inner_bag));
+    }
+  }
+  output << total_bag_contents("shiny gold", contains) << std::endl;
+}
