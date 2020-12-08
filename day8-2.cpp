8a461ec7bf30fd1aff238b60974fb6a303c8d3a0
Martynas Janonis <martynas.janonis.17@ucl.ac.uk>
2020-12-08 23:33:58 +0000

    Day 8 part 2 completed

---
+++ day8-2.cpp
@@ -0,0 +1,62 @@
+#include <fstream>
+#include <set>
+#include <string>
+#include <vector>
+
+int main()
+{
+  std::ifstream input{"day8.in"};
+  std::ofstream output{"day8-2.out"};
+
+  std::vector<std::pair<std::string, int>> program;
+  std::string tmp;
+
+  while (getline(input, tmp)) {
+    std::string op = tmp.substr(0, tmp.find(' '));
+    int arg = std::stoi(tmp.substr(tmp.find(' ') + 1));
+    program.push_back(std::make_pair(op, arg));
+  }
+
+  std::set<int> visited_instr;
+  int ip = 0, acc = 0;
+
+  std::vector<std::pair<std::string, int>> program_orig{program};
+
+  for (int i = 0; i < program_orig.size(); ++i) {
+    if (program_orig[i].first == "acc") {
+      continue;
+    }
+    ip = 0;
+    acc = 0;
+    visited_instr.clear();
+    if (program[i].first == "jmp") {
+      program[i].first = "nop";
+    }
+    else {
+      program[i].first = "jmp";
+    }
+
+    while (ip < program.size()) {
+      if (visited_instr.find(ip) != visited_instr.end()) {
+        break;
+      }
+      visited_instr.insert(ip);
+      if (program[ip].first == "nop") {}
+      else if (program[ip].first == "acc") {
+        acc += program[ip].second;
+      }
+      else if (program[ip].first == "jmp") {
+        ip += program[ip].second;
+        continue;
+      }
+      ++ip;
+    }
+
+    if (ip == program.size()) {
+      break;
+    }
+    program = program_orig;
+  }
+
+  output << acc << std::endl;
+}
