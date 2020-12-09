50b86470f2a5e7681e768bc01a5e156e95f51f53
Martynas Janonis <martynas.janonis.17@ucl.ac.uk>
2020-12-09 18:44:03 +0000

    Day 9 part 1 completed

---
+++ day9-1.cpp
@@ -0,0 +1,37 @@
+#include <fstream>
+#include <list>
+#include <unordered_map>
+
+int main()
+{
+  std::ifstream input{"day9.in"};
+  std::ofstream output{"day9-1.out"};
+
+  std::list<long int> data;
+  long int tmp = 0;
+  // Get the preamble
+  for (int i = 0; i < 25; ++i) {
+    input >> tmp;
+    data.push_back(tmp);
+  }
+
+  std::unordered_map<long int, long int> hmap;
+
+  while (input >> tmp) {
+    bool pair_found = false;
+    hmap.clear();
+    for (auto x : data) {
+      if (hmap.find(tmp - x) != hmap.end()) {
+        pair_found = true;
+        break;
+      }
+      hmap[x] = 1;
+    }
+    if (!pair_found) {
+      output << tmp << std::endl;
+      break;
+    }
+    data.pop_front();
+    data.push_back(tmp);
+  }
+}
