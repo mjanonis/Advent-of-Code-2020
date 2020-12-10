1b4738d69a543484e279a5140902edd77b269b9b
Martynas Janonis <martynas.janonis.17@ucl.ac.uk>
2020-12-10 16:23:00 +0000

    Day 10 part 1 completed

---
+++ day10-1.cpp
@@ -0,0 +1,31 @@
+#include <algorithm>
+#include <fstream>
+#include <unordered_map>
+#include <vector>
+
+int main()
+{
+  std::ifstream input{"day10.in"};
+  std::ofstream output{"day10-1.out"};
+
+  std::vector<unsigned int> adapters;
+  unsigned int tmp;
+  while (input >> tmp) {
+    adapters.push_back(tmp);
+  }
+
+  unsigned int device_joltage =
+      *std::max_element(adapters.begin(), adapters.end()) + 3;
+
+  unsigned int curr_jotalge = 0;
+  std::unordered_map<unsigned int, unsigned int> diff_count;
+  std::sort(adapters.begin(), adapters.end());
+
+  for (auto x : adapters) {
+    ++diff_count[x - curr_jotalge];
+    curr_jotalge = x;
+  }
+
+  ++diff_count[3];
+  output << diff_count[1] * diff_count[3] << std::endl;
+}
